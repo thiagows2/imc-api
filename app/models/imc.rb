2d726e31be3a750689ad37484c827afcf64a3fab
Thiago Marques <thiagomarques408@gmail.com>
2022-01-18 19:17:52 -0300

    gem byebug and imc calculator

--- app/models/imc.rb
+++ app/models/imc.rb
@@ -1,2 +1,18 @@
 class Imc < ApplicationRecord
+  IMC_CATEGORIES = %w[severely_underweight underweight healthy_weight overweight obesity].freeze
+  OBESITY = %w[I II III]
+
+  validates :height, :weight, presence: true
+
+  before_save :imc_calculator
+
+  def imc_calculator
+    self.imc = (weight / height ** 2).ceil(1)
+
+    define_classification
+  end
+
+  def define_classification
+    debugger
+  end
 end
